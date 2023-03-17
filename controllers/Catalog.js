6659576ca7b122cb8aed74fbf5082e65009e86cf
OSB-Onboarding <127765739+OSB-Onboarding@users.noreply.github.com>
2023-03-17 14:24:08 +0530

    Update Catalog.js

--- controllers/Catalog.js
+++ controllers/Catalog.js
@@ -4,6 +4,9 @@ var utils = require('../utils/writer.js');
 var Catalog = require('../service/CatalogService');
 
 module.exports.catalogGetUsingGET = function catalogGetUsingGET (req, res, next, xBrokerAPIVersion) {
+  console.log("********* CATALOG GET **********");
+  console.log(JSON.stringify(req.headers));
+  console.log(JSON.stringify(res));
   Catalog.catalogGetUsingGET(xBrokerAPIVersion)
     .then(function (response) {
       utils.writeJson(res, response);
